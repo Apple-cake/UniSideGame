c0b2c7937a731d90bfbaf7d5b626934a48274b10
Apple-cake <tongensai.kudasai@gmail.com>
2021-05-19 02:38:18 +0900

    ジャンプ処理追加

--- UniSideGame/Assets/Player/PlayerController.cs
+++ UniSideGame/Assets/Player/PlayerController.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Rigidbody2D変数
     Rigidbody2D rbody;
+    // 水平方向の入力
     float axisH = 0.0f;
+    // 移動速度
     public float speed = 3.0f;
+    // ジャンプ力
+    public float jump = 9.0f;
+    // 着地レイヤー
+    public LayerMask groundLayer;
+    // ジャンプ開始フラグ
+    bool goJump = false;
+    // 地上判定
+    bool onGround = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,20 +34,48 @@ public class PlayerController : MonoBehaviour
         if (axisH > 0.0f)
         {
             // 右向き
-            Debug.Log("右移動");
+            Debug.Log("右移動開始");
             transform.localScale = new Vector2(1, 1);
         }
         else if (axisH < 0.0f)
         {
             // 左向き
-            Debug.Log("左移動");
+            Debug.Log("左移動開始");
             transform.localScale = new Vector2(-1, 1);
+        }
+        // キャラクターをジャンプさせる
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
         }
     }
 
     void FixedUpdate()
     {
-        // プレイヤーの速度
-        rbody.velocity = new Vector2(speed * axisH, rbody.velocity.y);
+        // 地上判定
+        onGround = Physics2D.Linecast(transform.position, transform.position - (transform.up * 0.1f), groundLayer);
+        // 地上 or プレイヤーの速度が0ではない
+        if (onGround || axisH != 0)
+        {
+            // プレイヤーの速度を更新
+            rbody.velocity = new Vector2(speed * axisH, rbody.velocity.y);
+        }
+        // 地上 and ジャンプキーが押された
+        if (onGround && goJump)
+        {
+            // ジャンプさせる
+            Debug.Log("ジャンプ開始");
+            Vector2 jumpPw = new Vector2(0, jump);
+            rbody.AddForce(jumpPw, ForceMode2D.Impulse);
+            // ジャンプ開始フラグ false(= ジャンプ終了)
+            goJump = false;
+        }
+    }
+
+    public void Jump()
+    {
+        // ジャンプ開始フラグ true
+        goJump = true;
+        Debug.Log("ジャンプ開始");
     }
 }
